f5fc56a422ae7c95c67376c185dbcb77cd1cc4f0
suryana140802-oss <suryana140802@gmail.com>
2026-09-21 19:03:31 +0700

    Reset version to 1.0.0 for initial release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.flipz.manga"
     minSdk = 24
     targetSdk = 36
-    versionCode = 5
-    versionName = "2.3"
+    versionCode = 1
+    versionName = "1.0.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     ndk {
